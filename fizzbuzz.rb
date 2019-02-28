0c9ef1e497256b7e7e8b7c6aad933dfad6fe3f14
Tommi Cox <tommicox@gmail.com>
2019-02-28 23:49:25 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -7,7 +7,7 @@ def fizzbuzz(theNumber)
     if theNumber % 5 == 0 
       return "Buzz"
     end
-   if theNumber % 3 == 0 && theNumber % 5 == 0
+   if theNumber % 15 == 0 
      return "FizzBuzz"
   end
 end
