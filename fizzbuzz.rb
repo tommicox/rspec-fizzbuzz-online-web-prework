d0558382851fd1f50d6d713348f1c9ca13da2200
Tommi Cox <tommicox@gmail.com>
2019-02-28 23:41:22 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(theNumber)
-  if theNumber % 3 == "Fizz"
+  if theNumber % 3 == 0
     return "Fizz"
   end
-    if theNumber % 5 == "Buzz" 
+    if theNumber % 5 == 0 
       return "Buzz"
     end
-   if theNumber  % 15 == "FizzBuzz"
+   if theNumber  % 15 == 0
      return "FizzBuzz"
   end
 end
